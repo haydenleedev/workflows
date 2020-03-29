eceebc1adb8537fcb0dc4b82a592ee4dec282918
Hayden Lee <haydenlee22@gmail.com>
2020-03-29 11:30:06 -0400

    Installed gulp and gutil

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util');  // import gulp and gulp-util plugins into this file.
+
+gulp.task('log', function() {
+    gutil.log('Workflows are awesome')
+});
